f38553595f684d74677bca43a14231edbcefee6d
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-16 17:43:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,17 +1,10 @@
 function forLoop(array) { 
   for (let i = 0; i < 25; i++){
     if (i === 1) {
-    return "I am 1 strange loop";
+      return "I am 1 strange loop";
     } else {
       return `I am ${i} strange loops`;
     }
   }
 }
 
-if (count === 0) {
-  console.log('There are no students');
-} else if (count === 1) {
-  console.log('There is only one student');
-} else {
-  console.log(`There are ${count} students`);
-}
